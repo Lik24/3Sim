6b6b5fb4ce1ac33bb7cc9266b40c2611dec6af78
Lik24 <hammer-rav@mail.ru>
2014-09-17 15:50:48 +0600

    Проверить с разными dt
    Signed-off-by: Lik24 <hammer-rav@mail.ru>

--- 2be_0.663/Sim_Lib/SGim.m
+++ 2be_0.663/Sim_Lib/SGim.m
@@ -1,4 +1,4 @@
-function [Clp,Cwp,Cws,A,Bwo,Mp]=SGim(dV,SCw,dt,Mp,zc,Bwo,P,Patm,P0,va,vc,vg,vd)
+function [Clp,Cwp,Cws,A,Bwo,Mp]=SGim(dV,SCw,Mp,zc,Bwo,P,Patm,P0,va,vc,vg,vd)
 
 Bw=Bwo(:,1:2);
 Bo=Bwo(:,3:4);
@@ -9,10 +9,10 @@ Bo=Bwo(:,3:4);
 [dVg,Gw,G_mp,BwG,BoG]=Narez(dV,SCw,Mp,Bw,Bo,vg);
 [dVd,Dw,D_mp,BwD,BoD]=Narez(dV,SCw,Mp,Bw,Bo,vd);
 
-[CwpA,CopA,BwA,BoA,CwsA,AA,MpA]=ZaSgim(dVa,Sw,dt,A_mp,zc([1,2,4]),P(va),Patm,P0(va),BwA,BoA);
-[CwpC,CopC,BwC,BoC,CwsC,AC,MpC]=ZaSgim(dVc,Cw,dt,C_mp,zc([1,2,4]),P(vc),Patm,P0(vc),BwC,BoC);
-[CwpG,CopG,BwG,BoG,CwsG,AG,MpG]=ZaSgim(dVg,Gw,dt,G_mp,zc([1,2,4]),P(vg),Patm,P0(vg),BwG,BoG);
-[CwpD,CopD,BwD,BoD,CwsD,AD,MpD]=ZaSgim(dVd,Dw,dt,D_mp,zc([1,2,6]),P(vd),Patm,P0(vd),BwD,BoD);
+[CwpA,CopA,BwA,BoA,CwsA,AA,MpA]=ZaSgim(dVa,Sw,A_mp,zc([1,2,4]),P(va),Patm,P0(va),BwA,BoA);
+[CwpC,CopC,BwC,BoC,CwsC,AC,MpC]=ZaSgim(dVc,Cw,C_mp,zc([1,2,4]),P(vc),Patm,P0(vc),BwC,BoC);
+[CwpG,CopG,BwG,BoG,CwsG,AG,MpG]=ZaSgim(dVg,Gw,G_mp,zc([1,2,4]),P(vg),Patm,P0(vg),BwG,BoG);
+[CwpD,CopD,BwD,BoD,CwsD,AD,MpD]=ZaSgim(dVd,Dw,D_mp,zc([1,2,6]),P(vd),Patm,P0(vd),BwD,BoD);
 
 Bwo(:,1:2)=[BwA;BwC;BwG;BwD];
 Bwo(:,3:4)=[BoA;BoC;BoG;BoD];
@@ -29,15 +29,15 @@ Mp=[MpA;MpC;MpG;MpD];
 
 end
 
-function [Cwp,Cop,Bw,Bo,Cws,A,Mp]=ZaSgim(dV,Sw,dt,mp,zc,P,Patm,P0,Bw,Bo)
+function [Cwp,Cop,Bw,Bo,Cws,A,Mp]=ZaSgim(dV,Sw,mp,zc,P,Patm,P0,Bw,Bo)
     
     Bw2=Bw(:,1)./(1+zc(1)*(P-Patm));
     Bo2=Bo(:,1)./(1+zc(2)*(P-Patm));
     
     mp2=mp(:,1).*(1+zc(3)*(P-P0));
     
-    Cwp=dV.*Sw/dt.*(mp(:,2)*zc(3)./Bw2+mp2*zc(1)./Bw(:,2));
-    Cop=dV.*(1-Sw)/dt.*(mp(:,2)*zc(3)./Bo2+mp2*zc(2)./Bo(:,2));
+    Cwp=dV.*Sw.*(mp(:,2)*zc(3)./Bw2+mp2*zc(1)./Bw(:,2));
+    Cop=dV.*(1-Sw).*(mp(:,2)*zc(3)./Bo2+mp2*zc(2)./Bo(:,2));
     
     Cws=dV.*mp2./Bw2;
     A=Bw2./Bo2;
--- 2be_0.663/Termal_lib/SimT_MKT.m
+++ 2be_0.663/Termal_lib/SimT_MKT.m
@@ -94,7 +94,7 @@ j=0;
 PpW=zeros(size(Uf));
 
 [CR_rc]=Pre_Crack(RC,na,TM,A2C,A2G,Wf,Won,WonM,nw);
-%[CR]=SS_ind(RC);
+[CR]=SS_ind(RC);
 
 
 C2=C;
@@ -152,12 +152,9 @@ fp=1;
      
      kfw(na+1:na+nc+ng,1)=Sat_cal([Cw(:,t);Gw(:,t)],1,1,ts,tw); %water
      kfo(na+1:na+nc+ng,1)=Sat_cal([Cw(:,t);Gw(:,t)],2,1,ts,tw); %oil
-t
-% if t==2
-%     jhfgjhfg
-% end;
+
     [b1gm,b1gc,b1gg,b2gm]=GY_bild(GYData,Pi(1:na,1),Sw(:,1),BZ,na,nc,ng,T_GY,as,aw,mu);
-    [Clp,Cwp,Cws,A,Bwo,Mp]=SGim(dV./Mp(:,1),SCw,dt,Mp,zc,Bwo,Pi,1,P0,va,vc,vg,vd);
+    [Clp,Cwp,Cws,A,Bwo,Mp]=SGim(dV./Mp(:,1),SCw,Mp,zc,Bwo,Pi,1,P0,va,vc,vg,vd);
 
     [TL,TW,TP]=Potok_MKT(TM,Pi(1:na,1),kfw(1:na),kfo(1:na),MCp(:,1),as,aw,mu,RC.Arc,mup,fp,kms(1),L,Ke,Ro,A(va));
     [CL,~,~]=Potok_Tube(TC,Pi(vc,1),Cw(:,t),CCp(:,t),PR,mup,fp,kms(2),DATA.Lc,RC.Cr2,RC.Cc2,nc,A(vc));
@@ -230,7 +227,7 @@ t
  Qf=Qz1;
  Sw2=Sw;
  [SCw,SCp,NDT(t),Q1,Q2,Qm1,dSS(t)]=Sat_fast_2(SCw,SCp,RC,TC,TG,TA2C,TA2G,Pi(:,1),PR,ndt,Won,Wf,...
-     Uf(:,ft+1),dt,dVCG,Pw(:,ft+1),WonG,CpW(:,ft+1),WonC,Nl,CR_rc,Qz1,Qf,Pi0,TW,W6,TP,W7,L,DATA.Lc,DATA.Lg,Ke,Cws,Cwp);
+     Uf(:,ft+1),dt,dVCG,Pw(:,ft+1),WonG,CpW(:,ft+1),WonC,Nl,CR_rc,CR,Qz1,Qf,Pi0,TW,W6,TP,W7,L,DATA.Lc,DATA.Lg,Ke,Cws,Cwp);
     
     Sw(:,1)=SCw(1:na);
     Cw(:,t+1)=SCw(na+1:na+nc);
